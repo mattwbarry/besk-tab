00fdfbbb796affd980a9b39b24167b7d55ce9080
unknown <mattwbarry@gmail.com>
2014-08-29 15:41:17 -0400

    art, settings basic functionality, holder background

--- func.js
+++ func.js
@@ -1,11 +1,13 @@
-// set time to update every second
-setInterval(function() {
-	setTime()
-}, 1000)
+var name
+var city
+var state
 
 function setTime() {
 	var d = new Date()
-	var hours = d.getHours() % 12
+	var hours = d.getHours()
+	if (getLocal('time-format') != '24') {
+		hours %= 12
+	}
 	var minutes = d.getMinutes()
 	if (minutes < 10) { minutes = '0' + minutes}
 	document.getElementById('time').innerHTML = hours + ':' + minutes
@@ -18,18 +20,87 @@ function goToApps() {
 }
 
 function save(k, v) {
-	chrome.storage.local.set({k:v})
+	localStorage[k] = v
 }
 
-// initialize everything when ready
-$(document).ready(function() {
-	
-	// set initial time
+function getLocal(k) {
+	return localStorage[k]
+}
+
+function reset() {
+	localStorage.clear()
+}
+
+// initialize normal functionality
+function init() {
+	$('.init').remove()
+	$('.load').css('display', 'block')
 	setTime()
+
+	// initialize settings
+	$('#s-name').val(getLocal('name'))
+	$('#s-city').val(getLocal('city'))
+	$('#s-state').val(getLocal('state'))
+	$('#s-time-format').html(getLocal('time-format') + ' hour clock')
 
 	// apps button listener
 	var btnApps = document.getElementById('btn-apps')
 	btnApps.addEventListener('click', goToApps)
+
+	// get weather
+	$.simpleWeather({
+		location: getLocal('city') + ', ' + getLocal('state'),
+		woeid: '',
+		unit: 'f',
+		success: function(weather) {
+	    	$('#weather-temp').html(weather.temp)
+			$('#weather-conditions').html(weather.currently + ' in ' + weather.city)
+		}
+	})
+}
+
+function ask() {
+	$('#q-name').css('display', 'block')
+
+	// set initial starting vars
+	save('time-format', '12')
+
+}
+
+function closeSettings(e) {
+	if ($(e.target).closest('#settings-wrapper').length == 0) {
+		$('#settings-wrapper').animate({
+			'right': '-200px'
+		}, 200)
+		$(window).unbind('click', closeSettings)
+	}
+}
+
+// set time to update every second
+setInterval(function() {
+	setTime()
+}, 1000)
+
+// listeners
+$(document).on('keypress', '#a-name', function(e) {
+	if (e.which == 13 && $(this).val() != '') {
+		save('name', $(this).val())
+		$('#q-name').fadeOut()
+		$('#q-location').fadeIn()		
+	}
+})
+
+$(document).on('keypress', '#a-city, #a-state', function(e) {
+	if (e.which == 13 && $('#a-city').val() != '' && $('#a-state').val().length == 2) {
+		save('city', $('#a-city').val())
+		save('state', $('#a-state').val())
+		$('#q-location').fadeOut()
+		init()
+	}
+})
+
+// initialize everything when ready
+$(document).ready(function() {
 
 	// fade in all content
 	$('body').animate({
@@ -37,34 +108,38 @@ $(document).ready(function() {
 	}, 200)
 
 	// get name
-	chrome.storage.local.get('name', function(result) {
-		if ('name' in result) {
-			alert(result.name)
+	if ('name' in localStorage) {
+		init()
+	}
+	else {
+		ask()
+	}
+
+	// settings controls
+	$('#btn-settings').on('click', function() {
+		$('#settings-wrapper').animate({
+			'right': '0px'
+		}, 200)
+		setTimeout(function() {
+			$(window).bind('click', closeSettings)
+		}, 100)
+	})
+	$('.s-text').on('keypress', function(e) {
+		if (e.which == 13 && $(this).val() != '') {
+			$.each($('.s-text'), function() {
+				save($(this).attr('data-key'), $(this).val())
+			})
+			init()
+		}
+	})
+	$('#s-time-format').on('click', function() {
+		if (getLocal('time-format') == '24') {
+			save('time-format', '12')
+			$('#s-time-format').html('12 hour clock')
 		}
 		else {
-		//	prompt('what\'s your name?')
+			save('time-format', '24')
+			$('#s-time-format').html('24 hour clock')
 		}
-	})
-
-	// get city
-	chrome.storage.local.get('city', function(result) {
-		console.log(result)
-	})
-
-	// get state
-	chrome.storage.local.get('state', function(result) {
-		console.log(result)
-	})
-
-
-	// get weather
-	$.simpleWeather({
-	    location: 'Austin, TX',
-	    woeid: '',
-	    unit: 'f',
-	    success: function(weather) {
-	    	$('#weather-temp').html(weather.temp)
-	    	$('#weather-conditions').html(weather.currently + ' in ' + weather.city)
-	    }
 	})
 })
